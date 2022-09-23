374a466134d3ac3b2781b6c8213b399f0a77ab53
PJ Trowski <ptrowski@protonmail.com>
2022-09-23 08:43:06 -0400

    adding constant serviceist

---
+++ src/constans/services.js
@@ -0,0 +1,28 @@
+const servicesList=
+[
+{
+    serviceName1:"",
+    details1:"",
+    details2:"",
+    details3:"",
+    details4:"",
+    details5:"",
+
+},
+{
+    serviceName2:"",
+    details1:"",
+    details2:"",
+    details3:"",
+    details4:"",
+    details5:"",
+},
+{
+    serviceName3:"",
+    details1:"",
+    details2:"",
+    details3:"",
+    details4:"",
+    details5:"",
+}
+]
